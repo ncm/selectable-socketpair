82ef416726e2478e148c3ad3e55296a9e3537109
Ger Hobbelt <ger@hobbelt.com>
2012-04-23 01:34:11 +0200

    remove one compiler warning

--- socketpair.c
+++ socketpair.c
@@ -1,23 +1,23 @@
 /* socketpair.c
  * Copyright 2007, 2010 by Nathan C. Myers <ncm@cantrip.org>
- * This code is Free Software.  It may be copied freely, in original or 
+ * This code is Free Software.  It may be copied freely, in original or
  * modified form, subject only to the restrictions that (1) the author is
  * relieved from all responsibilities for any use for any purpose, and (2)
  * this copyright notice must be retained, unchanged, in its entirety.  If
  * for any reason the author might be held responsible for any consequences
- * of copying or use, license is withheld.  
+ * of copying or use, license is withheld.
  */
 
 /* Changes:
  * 2012-03-15:
- *   always init the socks[] to -1/INVALID_SOCKET on error, both on Win32/64 
+ *   always init the socks[] to -1/INVALID_SOCKET on error, both on Win32/64
  *   and UNIX/other platforms
  * 2010-03-31:
  *   set addr to 127.0.0.1 because win32 getsockname does not always set it.
  * 2010-02-25:
  *   set SO_REUSEADDR option to avoid leaking some windows resource.
- *   Windows System Error 10049, "Event ID 4226 TCP/IP has reached 
- *   the security limit imposed on the number of concurrent TCP connect 
+ *   Windows System Error 10049, "Event ID 4226 TCP/IP has reached
+ *   the security limit imposed on the number of concurrent TCP connect
  *   attempts."  Bleah.
  * 2007-04-25:
  *   preserve value of WSAGetLastError() on all error returns.
@@ -45,7 +45,7 @@
 /* dumb_socketpair:
  *   If make_overlapped is nonzero, both sockets created will be usable for
  *   "overlapped" operations via WSASend etc.  If make_overlapped is zero,
- *   socks[0] (only) will be usable with regular ReadFile etc., and thus 
+ *   socks[0] (only) will be usable with regular ReadFile etc., and thus
  *   suitable for use as stdin or stdout of a child process.  Note that the
  *   sockets must be closed with closesocket() regardless.
  */
@@ -69,16 +69,16 @@ int dumb_socketpair(SOCKET socks[2], int make_overlapped)
     socks[0] = socks[1] = INVALID_SOCKET;
 
     listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (listener == INVALID_SOCKET) 
+    if (listener == INVALID_SOCKET)
         return SOCKET_ERROR;
 
     memset(&a, 0, sizeof(a));
     a.inaddr.sin_family = AF_INET;
     a.inaddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-    a.inaddr.sin_port = 0; 
+    a.inaddr.sin_port = 0;
 
-    do {
-        if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, 
+    for (;;) {
+        if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR,
                (char*) &reuse, (socklen_t) sizeof(reuse)) == -1)
             break;
         if  (bind(listener, &a.addr, sizeof(a.inaddr)) == SOCKET_ERROR)
@@ -107,8 +107,7 @@ int dumb_socketpair(SOCKET socks[2], int make_overlapped)
 
         closesocket(listener);
         return 0;
-
-    } while (0);
+    }
 
     e = WSAGetLastError();
     closesocket(listener);
